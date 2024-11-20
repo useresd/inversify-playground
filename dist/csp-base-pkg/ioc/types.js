cb76140e8781d0187b7d5faf1207f7b915805490
Yousif Magdi <yousif.magdi@contellect.com>
2024-11-20 11:41:05 +0200

    chore: removed dist folder from git

--- dist/csp-base-pkg/ioc/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-const TYPES = {
-    UserInfo: Symbol.for("UserInfo"),
-    Logger: Symbol.for("Logger"),
-    TenantDetailsProvider: Symbol.for("TenantDetailsProvider")
-};
-exports.TYPES = TYPES;
--- dist/tasklist/ioc/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    CustomerService: Symbol.for("CustomerService"),
-    CustomerRepository: Symbol.for("CustomerRepository")
-};
